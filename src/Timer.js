8a2338faf4ac9a50ef0a61107bd56894ea46c598
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-20 16:22:56 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -19,7 +19,8 @@ class Timer extends Component {
     );
   }
   componentDidUpdate(){
-
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
   componentWillUnmount() {
